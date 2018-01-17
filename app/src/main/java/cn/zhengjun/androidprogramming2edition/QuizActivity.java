717a587cc93a62dc6f6c56fb6b6872a7c1543b3b
zhengjun1987hz <zhengjun1987@outlook.com>
2018-01-17 11:58:35 +0800

    《Android编程权威指南》第二版 第二章 Android与MVC设计模式 2018-1-17 11:58:29

--- app/src/main/java/cn/zhengjun/androidprogramming2edition/QuizActivity.java
+++ app/src/main/java/cn/zhengjun/androidprogramming2edition/QuizActivity.java
@@ -1,16 +1,23 @@
 package cn.zhengjun.androidprogramming2edition;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
+import cn.zhengjun.androidprogramming2edition.bean.Question;
+
 public class QuizActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btn_true;
     private Button btn_false;
     private TextView tv_question_text;
+    private ArrayList<Question> mQuestions;
+    private int mQuestionIndex = 0;
+    private Button btn_next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +25,19 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_quiz);
         btn_true = (Button) findViewById(R.id.btn_true);
         btn_false = (Button) findViewById(R.id.btn_false);
+        btn_next = (Button) findViewById(R.id.btn_next);
         tv_question_text = (TextView) findViewById(R.id.tv_question_text);
         btn_false.setOnClickListener(this);
         btn_true.setOnClickListener(this);
+        btn_next.setOnClickListener(this);
+
+        mQuestions = new ArrayList<>();
+        mQuestions.add(new Question(R.string.questions_ocean, true));
+        mQuestions.add(new Question(R.string.questions_mideast, false));
+        mQuestions.add(new Question(R.string.questions_africa, false));
+        mQuestions.add(new Question(R.string.questions_america, true));
+        mQuestions.add(new Question(R.string.questions_asia, true));
+        updateQuestion();
 
     }
 
@@ -29,11 +46,25 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
         System.out.println("v = " + v.getId());
         switch (v.getId()) {
             case R.id.btn_false:
-                Toast.makeText(this, R.string.correct, Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
                 break;
             case R.id.btn_true:
-                Toast.makeText(this, R.string.incorrect, Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
+                break;
+            case R.id.btn_next:
+                mQuestionIndex = (mQuestionIndex + 1) % mQuestions.size();
+                updateQuestion();
                 break;
         }
+    }
+
+    private void checkAnswer(boolean b) {
+        Toast.makeText(this, (b == mQuestions.get(mQuestionIndex).isAnswer()) ? R.string.correct : R.string.incorrect, Toast.LENGTH_SHORT).show();
+    }
+
+    private void updateQuestion() {
+        System.out.println("mQuestionIndex = " + mQuestions.get(mQuestionIndex).toString());
+        int questionTextResId = mQuestions.get(mQuestionIndex).getQuestionTextResId();
+        tv_question_text.setText(questionTextResId);
     }
 }
